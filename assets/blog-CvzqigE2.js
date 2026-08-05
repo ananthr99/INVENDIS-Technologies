4b24dd69986a66a071c3adf2b480acf124836bd9
ananthr99 <ananthr99@users.noreply.github.com>
2026-08-05 09:23:47 +0000

    deploy: 53b6d75fab9baee7a7389bb98bcf60c96a97eac1

--- assets/blog-CvzqigE2.js
+++ assets/blog-CvzqigE2.js
@@ -1,4 +1,4 @@
-import{i as e}from"./index-D4y3v_k_.js";var t=e(`calendar`,[[`path`,{d:`M8 2v4`,key:`1cmpym`}],[`path`,{d:`M16 2v4`,key:`4m81vk`}],[`rect`,{width:`18`,height:`18`,x:`3`,y:`4`,rx:`2`,key:`1hopcy`}],[`path`,{d:`M3 10h18`,key:`8toen8`}]]),n=Object.assign({"../content/blog/choosing-industrial-router-remote-sites.md":`---
+import{i as e}from"./index-C4oIp_JX.js";var t=e(`calendar`,[[`path`,{d:`M8 2v4`,key:`1cmpym`}],[`path`,{d:`M16 2v4`,key:`4m81vk`}],[`rect`,{width:`18`,height:`18`,x:`3`,y:`4`,rx:`2`,key:`1hopcy`}],[`path`,{d:`M3 10h18`,key:`8toen8`}]]),n=Object.assign({"../content/blog/choosing-industrial-router-remote-sites.md":`---
 title: "How to Choose the Right Industrial Router for Remote Site Connectivity"
 date: "2025-09-05"
 category: "Industry Insight"
